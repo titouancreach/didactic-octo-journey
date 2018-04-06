8998f84d9f0df160952c7fb4b362ab6ebe999102
titouan <titouan.creach@eegle.io>
2018-04-06 11:10:45 +0200

    add a monad for manage error and success

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,5 @@
+// @flow
+
 // credit to this guys: https://gist.github.com/gordonbrander/2230317
 // it should be safe enough for this app
 export const getUniqId = () =>
@@ -5,3 +7,54 @@ export const getUniqId = () =>
   Math.random()
     .toString(36)
     .substr(2, 9);
+
+
+// Simple Error/Success monad
+export const Success = (value) => {
+  return {
+    map(f) {
+      return Success(f(value))
+    },
+    flatMap(f) {
+      return f(value);
+    },
+    isSuccess() {
+      return true;
+    },
+    isError() {
+      return !this.isSuccess();
+    },
+    getValue() {
+      return value
+    },
+    fold(f/*, g*/) {
+      return f(value)
+    }
+  }
+};
+
+export const Error = error => {
+  return {
+    map() {
+      return Error(error)
+    },
+    flatMap() {
+      return Error(error);
+    },
+    isSuccess() {
+      return false;
+    },
+    isError() {
+      return !this.isSuccess()
+    },
+    getError() {
+      return error;
+    },
+    fold(f, g) {
+      return g(error)
+    }
+  }
+};
+
+
+export type Either = Error | Success;
