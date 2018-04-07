d555e8bff85757cd314a7ef4ed4cdbf378a144fb
Titouan CREACH <titouan.creach@eegle.io>
2018-04-07 21:07:16 +0200

    add our displayer component

---
+++ src/components/BookmarkEdit/BookmarkInfo.jsx
@@ -0,0 +1,13 @@
+// @flow
+
+import * as React from 'react';
+import FlickrInfo from './FlickInfo';
+import type {Bookmark} from '../../type';
+
+type Props = {
+  bookmark: Bookmark
+}
+
+export default ({bookmark}: Bookmark) => {
+  return <FlickrInfo bookmark={bookmark} />
+};
---
+++ src/components/BookmarkEdit/FlickInfo.jsx
@@ -0,0 +1,20 @@
+// @flow
+
+import * as React from 'react';
+import Grid from 'material-ui/Grid';
+import type {FlickrBookmark} from '../../type';
+import Typography from 'material-ui/Typography';
+
+type Props = {
+  bookmark: FlickrBookmark
+};
+
+
+export default ({bookmark}: Props) => {
+  return (
+    <React.Fragment>
+      <Grid item xs={12}> <Typography> Width: {bookmark.width}</Typography> </Grid>
+      <Grid item xs={12}> <Typography>Height: {bookmark.height} </Typography></Grid>
+     </React.Fragment>
+  )
+};
