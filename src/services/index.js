20223c7ce946a50ff9ecb5096128cee6ea2750be
Titouan CREACH <titouan.creach@eegle.io>
2018-04-07 22:33:09 +0200

    format

--- src/services/index.js
+++ src/services/index.js
@@ -2,14 +2,13 @@ import {get as flickrGet} from './flickr';
 import {get as vimeoGet} from './vimeo';
 import {Error, Success} from '../helpers';
 
-
 // Would be helpful to check for https and for domain extension
 export const getFetcher = url => {
   if (url.startsWith('https://www.flickr.com')) {
     return Success(flickrGet);
   }
   if (url.startsWith('https://vimeo.com')) {
-    return Success(vimeoGet)
+    return Success(vimeoGet);
   }
   return Error('Url should be either Vimeo or Flickr');
-}
+};
