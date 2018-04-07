ce26c55d5490d077031b9fc64e251ecc064ebaa0
Titouan CREACH <titouan.creach@eegle.io>
2018-04-07 18:07:26 +0200

    grid layout

--- src/routes/Bookmark/index.jsx
+++ src/routes/Bookmark/index.jsx
@@ -1,5 +1,47 @@
-import * as React from 'react';
+// @flow
 
-export default () => (
-  <div> I'm here babe </div>
-);
+import * as React from 'react';
+import {connect} from 'react-redux'; 
+import BookmarkNotFound from '../../components/NotFound/bookmark';
+import BookmarkEdit from '../../components/BookmarkEdit/index';
+import Grid from 'material-ui/Grid';
+import {withStyles} from 'material-ui/styles';
+
+const isValidBookmarkId = (bookmarkId, allBookmarkIds) => {
+  return !!allBookmarkIds.find(id => bookmarkId === id);
+}
+
+
+const style = {
+  root: {
+    margin: 64
+  }
+};
+
+const Bookmark = ({match, allBookmarkIds, classes}) => {
+
+  const {bookmarkId} = match.params;
+  
+  if (!isValidBookmarkId(bookmarkId, allBookmarkIds)) {
+    return (
+      <BookmarkNotFound />
+    );
+  }
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={24}>
+        <BookmarkEdit bookmarkId={bookmarkId} />
+      </Grid>
+    </div>
+    
+  );
+};
+
+const mapState = state => {
+  return {
+    allBookmarkIds: state.link.allIds
+  };
+}
+
+export default withStyles(style)(connect(mapState)(Bookmark));
