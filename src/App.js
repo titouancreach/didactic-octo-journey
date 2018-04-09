03730f66ffe2322fc8c16fe6398fc8a36b13423f
Titouan CREACH <titouan.creach@eegle.io>
2018-04-09 21:30:41 +0200

    change color scheme

--- src/App.js
+++ src/App.js
@@ -7,21 +7,33 @@ import {Provider} from 'react-redux';
 import store from './store';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Bookmark from './routes/Bookmark/index';
+import {MuiThemeProvider, createMuiTheme} from 'material-ui/styles';
+import blue from 'material-ui/colors/blue';
+import pink from 'material-ui/colors/pink';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+    secondary: pink
+  }
+});
 
 class App extends React.Component<{}> {
   render() {
     return (
-      <Provider store={store}>
-        <Router>
-          <div>
-            <AppBar title="Bookmark" />
+      <MuiThemeProvider theme={theme}>
+        <Provider store={store}>
+          <Router>
             <div>
-              <Route exact path="/" component={Home} />
-              <Route path="/bookmark/:bookmarkId" component={Bookmark} />
+              <AppBar title="Bookmark" />
+              <div>
+                <Route exact path="/" component={Home} />
+                <Route path="/bookmark/:bookmarkId" component={Bookmark} />
+              </div>
             </div>
-          </div>
-        </Router>
-      </Provider>
+          </Router>
+        </Provider>
+      </MuiThemeProvider>
     );
   }
 }
